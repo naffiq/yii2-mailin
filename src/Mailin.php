2fccea2d8929d77330a7807e26677c8d02e0261e
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2016-12-07 12:13:01 +0600

    Init code commit

---
+++ src/Mailin.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace naffiq\mailin;
+
+use yii\base\Component;
+
+/**
+ * Class Mailin
+ *
+ * Used as a component wrapper for SendinBlue Mailin PHP Library
+ *
+ * @package naffiq\mailin
+ */
+class Mailin extends Component
+{
+    /**
+     *
+     * @var string
+     */
+    public $apiKey;
+
+    /**
+     * @var string
+     */
+    public $apiUrl = 'https://api.sendinblue.com/v2.0';
+
+    /**
+     * @var \Sendinblue\Mailin
+     */
+    private $mailin;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->mailin = new \Sendinblue\Mailin($this->apiUrl, $this->apiUrl);
+    }
+
+    /**
+     * @return \Sendinblue\Mailin
+     */
+    public function getClient()
+    {
+        return $this->mailin;
+    }
+}
